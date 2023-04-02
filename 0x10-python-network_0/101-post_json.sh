9a2b683fe6e4a590fdef5ad1cdf1b63f59e6942e
Gavilite <111202565+Gavilite@users.noreply.github.com>
2023-04-02 11:57:05 -0700

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends request to POST to URL and display the body of response
-curl -s -d "$2" -H "Content_Type: application/json" "$1"
+# sends a JSON POST request to a URL, and displays the body of the response.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
