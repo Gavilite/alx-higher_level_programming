9f579368a38b86d44e9c0f75389ad55714584ce7
Gavilite <111202565+Gavilite@users.noreply.github.com>
2023-04-02 12:01:13 -0700

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
-""" Find the peak of a list"""
+""" Finds a peak in a list of unsorted integers
+"""
 
 
-def find_peak(my_list):
-    """ Returns the peak value in the list """
-    if len(my_list) == 0:
+def find_peak(list_of_integers):
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
+    """
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
         return None
-    if len(my_list) == 1:
-        return my_list[0]
-    return max(my_list)
+
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
+        else:
+            return list_of_integers[mid]
